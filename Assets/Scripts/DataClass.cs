6161dfc35edd36274a55ccfda5491a593c7032cb
jordandubreuil <35572152+jordandubreuil@users.noreply.github.com>
2024-02-08 14:32:18 -0500

    Added Unity Files

---
+++ Assets/Scripts/DataClass.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class DataClass 
+{
+    public int level;
+    public float timeElapsed;
+    public string name;
+}
---
+++ Assets/Scripts/JsonSerializer.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using TMPro;
+using UnityEngine.Networking;
+
+
+public class JsonSerializer : MonoBehaviour
+{
+    public DataClass dataObj;
+    public string filePath;
+    public TMP_InputField playerName, level, elapsedTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        filePath = Path.Combine(Application.dataPath, "saveData.txt");
+        dataObj = new DataClass();
+        dataObj.level = 1;
+        dataObj.timeElapsed = 44443.232323f;
+        dataObj.name = "Jordan";
+        string json = JsonUtility.ToJson(dataObj);
+        Debug.Log(json);
+        StartCoroutine(SendWebData(json));
+        File.WriteAllText(filePath, json);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SendButton()
+    {
+        var levelData = int.Parse(level.text);
+        var timeData = float.Parse(elapsedTime.text);
+        DataClass formData = new DataClass();
+        formData.level = levelData;
+        formData.timeElapsed = timeData;    
+        formData.name = playerName.text;
+        string json = JsonUtility.ToJson(formData);
+        filePath = Path.Combine(Application.dataPath, "formData.txt");
+        File.WriteAllText(filePath, json);
+        StartCoroutine(SendWebData(json));
+
+    }
+
+    public void GetButton() {
+        StartCoroutine(GetRequest("http://localhost:3000/getUnity"));
+    }
+
+    IEnumerator SendWebData(string json)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Post("http://localhost:3000/unity", json, "application-json"))
+        {
+            request.SetRequestHeader("content-type", "application/json");
+
+            request.uploadHandler.contentType = "application/json";
+            request.uploadHandler = new UploadHandlerRaw(System.Text.Encoding.UTF8.GetBytes(json));
+
+            yield return request.SendWebRequest();
+
+            if(request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                Debug.Log("Data Posted");
+            }
+            request.uploadHandler.Dispose();
+        }
+    }
+
+    IEnumerator GetRequest(string uri)
+    {
+        using (UnityWebRequest getRequest = UnityWebRequest.Get(uri))
+        {
+            yield return getRequest.SendWebRequest();
+
+            var newData = System.Text.Encoding.UTF8.GetString(getRequest.downloadHandler.data);
+            Debug.Log(newData);
+            var newGetRequestData = JsonUtility.FromJson<DataClass>(newData);
+
+            Debug.Log(newGetRequestData.name);
+            Debug.Log(newGetRequestData.level);
+            Debug.Log(newGetRequestData.timeElapsed);
+        }
+    }
+}
